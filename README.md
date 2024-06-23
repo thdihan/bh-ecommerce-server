32f2017a6e56a86d353d67703879e133920d978b
thdihan <thdihan@outlook.com>
2024-06-23 20:55:10 +0600

    Single product add test: Passed

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # BH - Ecommerce Server
+
+## Product Api
+
+-   Create Product `/api/products/create-product`
+
+Request Body:
+
+```json
+{
+    "productCode": "123456",
+    "name": "OnePlus Nord N30",
+    "price": 299.99,
+    "availableUnit": 150,
+    "description": "The OnePlus Nord N30 is a sleek and powerful smartphone offering great performance and value.",
+    "image": "https://www.startech.com.bd/image/cache/catalog/mobile/oneplus/nord-n30-se/nord-n30-se-01-500x500.png",
+    "categories": ["Electronics", "Mobile Phones", "Smartphones"]
+}
+```
+
+Response Example:
+
+```json
+{
+    "success": true,
+    "statusCode": 200,
+    "message": "Product created successfully",
+    "data": {
+        "productCode": "123456",
+        "name": "OnePlus Nord N30",
+        "price": 299.99,
+        "availableUnit": 150,
+        "description": "The OnePlus Nord N30 is a sleek and powerful smartphone offering great performance and value.",
+        "image": "https://www.startech.com.bd/image/cache/catalog/mobile/oneplus/nord-n30-se/nord-n30-se-01-500x500.png",
+        "categories": ["Electronics", "Mobile Phones", "Smartphones"],
+        "_id": "66783636b8ab38857f275670",
+        "createdAt": "2024-06-23T14:50:30.657Z",
+        "updatedAt": "2024-06-23T14:50:30.657Z",
+        "__v": 0
+    }
+}
+```
--- src/app/modules/products/products.model.ts
+++ src/app/modules/products/products.model.ts
@@ -1,37 +1,42 @@
 import { Schema, model } from 'mongoose';
 import { TProduct } from './products.interface';
 
-const productSchema = new Schema<TProduct>({
-    productCode: {
-        type: String,
-        required: true,
-        unique: true,
+const productSchema = new Schema<TProduct>(
+    {
+        productCode: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        availableUnit: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        categories: {
+            type: [String],
+            required: true,
+        },
     },
-    name: {
-        type: String,
-        required: true,
+    {
+        timestamps: true,
     },
-    price: {
-        type: Number,
-        required: true,
-    },
-    availableUnit: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    image: {
-        type: String,
-        required: true,
-    },
-    categories: {
-        type: [String],
-        required: true,
-    },
-});
+);
 
 export const ProductModel = model<TProduct>('Product', productSchema);
